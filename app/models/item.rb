b8a13c0b4d32ed20bdb5546643d603d800245f95
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-09 19:17:01 +0900

    バリデーションの設定（一部未設定）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  #バリデーションの設定
+  validates :name,                   presence: true
+  validates :info,                   presence: true
+  validates :category_id,            presence: true
+  validates :status_id,              presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :delivery_scheduled_id,  presence: true
+  validates :price,                  presence: true
+  validates :image,                  presence: true
 end
