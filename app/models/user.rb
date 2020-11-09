251ced3fd0a23ae545e994d2db3364bfce0f73a7
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-09 12:44:38 +0900

    記述の簡略化

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,14 @@ class User < ApplicationRecord
   validates :email,              uniqueness: { case_sensitive: true } #DB側は未設定
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  #validates :encrypted_password, confirmation: true, format: { with: /\A[a-z][\d]\Z/}
   
   #新規登録/本人情報確認
-  validates :last_name_hira, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name_hira,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :last_name_kata, presence: true, format: { with:/\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kata,  presence: true, format: { with:/\A[ァ-ヶー－]+\z/ }
-  validates :day_of_birthday,  presence: true
+  with_options presence: true do
+    validates :last_name_hira,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name_hira, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :last_name_kata,  format: { with:/\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kata, format: { with:/\A[ァ-ヶー－]+\z/ }
+    validates :day_of_birthday
+  end
 
 end
