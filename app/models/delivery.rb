64f50fc30d44545acf7e203ec8f28b2475b42af3
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-10 13:59:26 +0900

    delivery_scheduleのリスト作成

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,8 @@
-class Delivery < ApplicationRecord
+class Delivery < ActiveHash::Base
+  self.date = [
+    { id: 1, name: '---' },
+    { id: 2, name: '1~2日で発送' },
+    { id: 3, name: '2~3日で発送' },
+    { id: 4, name: '4~7で発送' },
+  ]
 end
