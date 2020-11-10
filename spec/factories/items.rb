79dbec859d6c9927698709af07e72ef7f08ee7c7
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-11 03:36:39 +0900

    ダミーデータの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    name                   {Faker::Name.name}
+    info                   {Faker::Lorem.sentence}
+    category_id            {"3"}
+    status_id              {"2"}
+    shipping_fee_status_id {"2"}
+    prefecture_id          {"5"}
+    delivery_scheduled_id  {"2"}
+    price                  {"4000"}
+
+    association :user #ユーザーデータも同時に作成する為の記述
   end
 end
