e57d2f7b740099971d22ef62cc9a45edf5332901
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-12 21:00:47 +0900

    出品者以外は編集ページへ遷移できないよう設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id 
+      redirect_to root_path
+    end
   end
 
   private
