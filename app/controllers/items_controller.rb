4cef8ccd68ede4951f2e8c48d11491f71fa07a5e
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-12 19:19:48 +0900

    未ログインユーザーが詳細ページを閲覧できる様設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC") #一覧表示用の一時収納箱用意、並び順を降順へ
