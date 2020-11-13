4ae55ca1e7d4cf59abcab0e3161440462391b236
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-13 16:15:26 +0900

    不正な削除を防ぐ処理を記述/記述の簡略化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC") #一覧表示用の一時収納箱用意、並び順を降順へ
@@ -37,9 +37,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    unless current_user.id == @item.user_id 
+      redirect_to root_path
+    else
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
   private
