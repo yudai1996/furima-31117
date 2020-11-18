da63979f2d6ff85468082683d726586b52a82325
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-18 15:39:56 +0900

    扱いたいカラム名の記述

--- app/models/buy_info.rb
+++ app/models/buy_info.rb
@@ -1,3 +1,5 @@
 class BuyInfo
   include ActiveModel::Model
+  #ordersテーブルとresidencesテーブルのカラム名記述
+  attr_accessor :item_id, :user_id, :postal_code, :prefectures_id, :city, :house_number, :building, :phone_number, :order_id
 end
