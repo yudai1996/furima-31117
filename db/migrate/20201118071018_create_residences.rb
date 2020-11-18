b31ad534947630bf8c744f8bc56b99ef566f1852
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-18 17:05:35 +0900

    residencesテーブル作成

--- db/migrate/20201118071018_create_residences.rb
+++ db/migrate/20201118071018_create_residences.rb
@@ -1,7 +1,13 @@
 class CreateResidences < ActiveRecord::Migration[6.0]
   def change
     create_table :residences do |t|
-
+      t.string  :postal_code,    null: false
+      t.integer :prefectures_id, null: false
+      t.string  :city,           null: false
+      t.string  :house_number,   null: false
+      t.string  :building,       null: false
+      t.string  :phone_number,   null: false
+      t.integer :order_id,       foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_064619) do
+ActiveRecord::Schema.define(version: 2020_11_18_071018) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +51,18 @@ ActiveRecord::Schema.define(version: 2020_11_18_064619) do
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "residences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefectures_id", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "building", null: false
+    t.string "phone_number", null: false
+    t.integer "order_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
