425dd5fd27e7d3adb0f394502318d667adf30377
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-24 01:15:29 +0900

    バリデーションの追加

--- app/models/buy_info.rb
+++ app/models/buy_info.rb
@@ -16,11 +16,11 @@ class BuyInfo
   #バリデーションの設定(配送先住所)
   with_options presence: true do
     validates :postal_code,    format: {with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :prefecture_id,  numericality: { other_than: 1 }
     validates :city
     validates :house_number
     #任意入力 validates :building
-    validates :phone_number
+    validates :phone_number,   format: {with: /\A\d{11}\z/ }
   end
 
   #バリデーション(token)
