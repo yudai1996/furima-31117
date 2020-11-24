19e1ad07d06dc05b42d4e3e60c52efef98085b9b
yudai1996 <72481372+yudai1996@users.noreply.github.com>
2020-11-24 17:20:03 +0900

    条件分修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.order.present? || urrent_user.id == @item.user_id 
+    if @item.order.present? || current_user.id != @item.user_id 
       redirect_to root_path
     end
   end
